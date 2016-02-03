b0f59d37e7384b76cf2d22878957053bad9b0c5f
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-02-03 11:53:23 -0500

    try hiding the connection

--- database.js
+++ database.js
@@ -1,32 +1,32 @@
 var mysql = require('mysql');
 var db_config = require('./db-config.json');
 
-module.exports = {
+var connection = mysql.createConnection({
+  host: db_config.host,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database
+}),
 
-  connection: mysql.createConnection({
-    host: db_config.host,
-    user: db_config.user,
-    password: db_config.password,
-    database: db_config.database
-  }),
+module.exports = {
 
   write: function(q){
     var table = q.table;
     var data = q.data;
-    this.connection.connect(function(err){
+    connection.connect(function(err){
       if(err){
         console.error('error connecting ' + err.stack);
         return;
       } else {
-        console.log('connected as id ' + this.connection.threadId);
+        console.log('connected as id ' + connection.threadId);
       }
     });
     if(!Array.isArray(data)) {
       data = [data];
     }
     for(var i=0; i<data.length; i++){
-      this.connection.query('INSERT INTO ?? SET ?', [table, data], function(err, result){});
+      connection.query('INSERT INTO ?? SET ?', [table, data], function(err, result){});
     }
-    this.connection.end();
+    connection.end();
   }
 }
